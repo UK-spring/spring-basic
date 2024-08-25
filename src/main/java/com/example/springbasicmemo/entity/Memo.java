cd95d514debee7d56b8625194fda04599f61481f
UK <hwangwonuk94@gmail.com>
2024-08-26 01:34:19 +0900

    feat: Memo 클래스 설계

---
+++ src/main/java/com/example/springbasicmemo/entity/Memo.java
@@ -0,0 +1,12 @@
+package com.example.springbasicmemo.entity;
+
+import lombok.Getter;
+
+@Getter
+public class Memo {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+}
