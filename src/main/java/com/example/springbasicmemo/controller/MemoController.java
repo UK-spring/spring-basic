78a7c15d5a2fc2adaab627110743f0444968fb8a
UK <hwangwonuk94@gmail.com>
2024-08-26 03:01:28 +0900

    feat: 메모 생성 API 리팩토링 ResponseEntity

--- src/main/java/com/example/springbasicmemo/controller/MemoController.java
+++ src/main/java/com/example/springbasicmemo/controller/MemoController.java
@@ -3,34 +3,40 @@ package com.example.springbasicmemo.controller;
 import com.example.springbasicmemo.dto.MemoRequestDto;
 import com.example.springbasicmemo.dto.MemoResponseDto;
 import com.example.springbasicmemo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@RestController
+@RestController // @Controller + @ResponseBody
 @RequestMapping("/memos") // Prefix
 public class MemoController {
 
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto requestDto) {
 
         // MemoId 식별자 계산
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        // 요청받은 데이터로 Memo 객체 생성
         Memo memo = new Memo(memoId, requestDto.getTitle(), requestDto.getContents());
 
         // Inmemory DB에 Memo 저장
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
     }
 
     @GetMapping
     public List<MemoResponseDto> findAllMemos() {
 
+        // init List
         List<MemoResponseDto> responseList = new ArrayList<>();
 
+        // HashMap<Memo> -> List<MemoResponseDto>
         for (Memo memo : memoList.values()) {
             MemoResponseDto responseDto = new MemoResponseDto(memo);
             responseList.add(responseDto);
@@ -39,6 +45,20 @@ public class MemoController {
         // Map To List
 //        responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
         return responseList;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
+
+        // id가 없다면?
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
 }
