16c20f941a81dd06a69209a18befb3a3f2e6dc92
UK <hwangwonuk94@gmail.com>
2024-08-26 01:31:51 +0900

    init

---
+++ src/main/java/com/example/springbasicmemo/SpringBasicMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicmemo/SpringBasicMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
